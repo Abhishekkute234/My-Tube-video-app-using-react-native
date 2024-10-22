525239b913551753abd64f5b009bd68276eb5d1c
Abhishekkute234 <kuteabhishek2004@gmail.com>
2024-10-23 01:15:25 +0530

    adding font styles

--- native_app/app/index.jsx
+++ native_app/app/index.jsx
@@ -6,7 +6,7 @@ import { Link } from "expo-router";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Abhi's Project</Text>
+      <Text className="text-3xl font-pbold ">Abhi's Project</Text>
       <StatusBar style="auto" />
       <Link href="./profile" style={{ color: "blue" }}>
         GO to the profile
